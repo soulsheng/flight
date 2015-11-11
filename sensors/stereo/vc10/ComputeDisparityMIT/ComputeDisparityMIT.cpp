c05070e8f89fc68adbe4b6d142182a4792ad6b0a
ss <s>
2015-11-11 11:12:31 +0800

    get result, ready to verify

--- sensors/stereo/vc10/ComputeDisparityMIT/ComputeDisparityMIT.cpp
+++ sensors/stereo/vc10/ComputeDisparityMIT/ComputeDisparityMIT.cpp
@@ -11,10 +11,10 @@ using namespace cv;
 #define ConfigFile	"aaazzz.conf"
 #define CalibrationDir	"..\\..\\calib-02-20-2014"
 
-int configCD(PushbroomStereoState& state) 
+int configCD(OpenCvStereoCalibration& stereoCalibration, PushbroomStereoState& state) 
 {
 	// load calibration
-	OpenCvStereoCalibration stereoCalibration;
+	//OpenCvStereoCalibration stereoCalibration;
 
 	if (LoadCalibration(CalibrationDir, &stereoCalibration) != true)
 	{
@@ -57,6 +57,11 @@ int configCD(PushbroomStereoState& state)
 	return 0;
 }
 
+void DisplayPixelBlocks(Mat left_image, Mat right_image, int left, int top, 
+	PushbroomStereoState state, PushbroomStereo *pushbroom_stereo);
+
+void DrawLines(Mat leftImg, Mat rightImg, Mat stereoImg, 
+	int lineX, int lineY, int disparity, int inf_disparity);
 
 int main( )
 {
@@ -69,12 +74,180 @@ int main( )
 	cv::vector<Point3i> pointVector2d_inf; // for display
 
 
+	OpenCvStereoCalibration stereoCalibration;
 	PushbroomStereoState state; // HACK
-	return configCD(state);
+	configCD( stereoCalibration, state);
 
 	PushbroomStereo pushbroom_stereo;
 	pushbroom_stereo.ProcessImages(matL, matR, &pointVector3d, &pointColors, &pointVector2d, state);
 
+	// output
+	Mat matDisp, remapL, remapR;
+	bool show_display = true;
+	if (show_display) {
+		// we remap again here because we're just in display
+		Mat remapLtemp(matL.rows, matL.cols, matL.depth());
+		Mat remapRtemp(matR.rows, matR.cols, matR.depth());
+
+		remapL = remapLtemp;
+		remapR = remapRtemp;
+
+		remap(matL, remapL, stereoCalibration.mx1fp, Mat(), INTER_NEAREST);
+		remap(matR, remapR, stereoCalibration.mx2fp, Mat(), INTER_NEAREST);
+
+		remapL.copyTo(matDisp);
+
+		//process LCM until there are no more messages
+		// this allows us to drop frames if we are behind
+	} // end show_display
+
+	// global for where we are drawing a line on the image
+	int lineLeftImgPosition = -1;
+	int lineLeftImgPositionY = -1;
+	bool visualize_stereo_hits = true;
+	bool show_unrectified = false;
+
+	if (show_display) {
+
+		for (unsigned int i=0;i<pointVector2d.size();i++) {
+			int x2 = pointVector2d[i].x;
+			int y2 = pointVector2d[i].y;
+			//int sad = pointVector2d[i].z;
+			rectangle(matDisp, Point(x2,y2), Point(x2+state.blockSize, y2+state.blockSize), 0,  CV_FILLED);
+			rectangle(matDisp, Point(x2+1,y2+1), Point(x2+state.blockSize-1, y2-1+state.blockSize), 255);
+
+		}
+
+		// draw pixel blocks
+		if (lineLeftImgPosition >= 0 && lineLeftImgPositionY > 1) {
+			DisplayPixelBlocks(remapL, remapR, lineLeftImgPosition - state.blockSize/2, lineLeftImgPositionY - state.blockSize/2, state, &pushbroom_stereo);
+		}
+
+		// draw a line for the user to show disparity
+		DrawLines(remapL, remapR, matDisp, lineLeftImgPosition, lineLeftImgPositionY, state.disparity, state.zero_dist_disparity);
+
+
+		if (visualize_stereo_hits == true) {
+
+			// draw the points on the unrectified image (to see these
+			// you must pass the -u flag)
+			Draw3DPointsOnImage(matL, &pointVector3d, stereoCalibration.M1, stereoCalibration.D1, stereoCalibration.R1, 128);
+
+		}
+
+	}
+
+	if (show_unrectified == false) {
+
+		imshow("Input", remapL);
+		imshow("Input2", remapR);
+	} else {
+		imshow("Input", matL);
+		imshow("Input2", matR);
+	}
+
+	waitKey();
+
 	return 0;
 }
 
+
+/**
+ * Displays a very zoomed in version of the two pixel blocks being looked at
+ *
+ * @param left_image the left image
+ * @param right_image the right image
+ * @param left left coordinate of the box
+ * @param top top coordinate of the box
+ * @param state PushbroomStereoState containing stereo information
+ * @param pushbroom_stereo stereo object so we can run GetSAD
+ *
+ */
+void DisplayPixelBlocks(Mat left_image, Mat right_image, int left, int top, PushbroomStereoState state, PushbroomStereo *pushbroom_stereo) {
+    if (left + state.blockSize > left_image.cols || top+state.blockSize > left_image.rows
+        || left + state.blockSize > right_image.cols || top+state.blockSize > right_image.rows
+        || left + state.disparity < 0) { // remember, disparity can be negative
+
+        // invalid spot
+
+        return;
+    }
+
+    Mat left_block = left_image.rowRange(top, top+state.blockSize).colRange(left, left+state.blockSize);
+    Mat right_block = right_image.rowRange(top, top+state.blockSize).colRange(left+state.disparity, left+state.disparity+state.blockSize);
+
+
+    Mat laplacian_left;
+    Laplacian(left_image, laplacian_left, -1, 3, 1, 0, BORDER_DEFAULT);
+
+    Mat laplacian_right;
+    Laplacian(right_image, laplacian_right, -1, 3, 1, 0, BORDER_DEFAULT);
+
+    // compute stats about block
+    int left_interest, right_interest, raw_sad;
+    int sad = pushbroom_stereo->GetSAD(left_image, right_image, laplacian_left, laplacian_right, left, top,
+        state, &left_interest, &right_interest, &raw_sad);
+
+    float diff_score = 100*(float)abs(left_interest - right_interest)/(float)(left_interest+right_interest);
+
+    // make the blocks visible by making them huge
+    const int scale_factor = 100;
+
+    Size output_size = Size(state.blockSize * scale_factor, state.blockSize * scale_factor);
+    resize(left_block, left_block, output_size, 0, 0, INTER_NEAREST);
+    resize(right_block, right_block, output_size, 0, 0, INTER_NEAREST);
+
+    char sad_str_char[100], left_interest_char[100], right_interest_char[100],
+        diff_score_char[100], raw_sad_char[100];
+    sprintf(sad_str_char, "SAD = %d", sad);
+    sprintf(left_interest_char, "Left interest = %d", left_interest);
+    sprintf(right_interest_char, "Right interest = %d", right_interest);
+    sprintf(raw_sad_char, "Raw sad = %d", raw_sad);
+    sprintf(diff_score_char, "Diff score = %.02f", diff_score);
+
+    putText(right_block, raw_sad_char, Point(310, 425), FONT_HERSHEY_PLAIN, 1, 0);
+    putText(right_block, sad_str_char, Point(400, 445), FONT_HERSHEY_PLAIN, 1, 0);
+    putText(right_block, left_interest_char, Point(310, 465), FONT_HERSHEY_PLAIN, 1, 0);
+    putText(right_block, right_interest_char, Point(305, 485), FONT_HERSHEY_PLAIN, 1, 0);
+    putText(right_block, diff_score_char, Point(305, 495), FONT_HERSHEY_PLAIN, 1, 0);
+
+    imshow("Left Block", left_block);
+    imshow("Right Block", right_block);
+
+
+
+}
+
+
+/**
+ * Draws lines on the images for stereo debugging.
+ *
+ * @param rightImg right image
+ * @param stereoImg stereo image
+ * @param lineX x position of the line to draw
+ * @param lineY y position of the line to draw
+ * @param disparity disparity move the line on the right image over by
+ * @param inf_disparity disparity corresponding to "infinite distance"
+ *  used to filter out false-positives.  Usually availible in
+ *   state.zero_dist_disparity.
+ */
+void DrawLines(Mat leftImg, Mat rightImg, Mat stereoImg, int lineX, int lineY, int disparity, int inf_disparity) {
+    int lineColor = 128;
+    if (lineX >= 0)
+    {
+        // print out the values of the pixels where they clicked
+        //cout << endl << endl << "Left px: " << (int)leftImg.at<uchar>(lineY, lineX)
+        //    << "\tRight px: " << (int)rightImg.at<uchar>(lineY, lineX + disparity)
+        //    << endl;
+
+        line(leftImg, Point(lineX, 0), Point(lineX, leftImg.rows), lineColor);
+        line(stereoImg, Point(lineX, 0), Point(lineX, leftImg.rows), lineColor);
+        line(rightImg, Point(lineX + disparity, 0), Point(lineX + disparity, rightImg.rows), lineColor);
+
+        line(rightImg, Point(lineX + inf_disparity, 0), Point(lineX + inf_disparity, rightImg.rows), lineColor);
+
+        line(leftImg, Point(0, lineY), Point(leftImg.cols, lineY), lineColor);
+        line(stereoImg, Point(0, lineY), Point(leftImg.cols, lineY), lineColor);
+        line(rightImg, Point(0, lineY), Point(rightImg.cols, lineY), lineColor);
+    }
+}
